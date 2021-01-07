f2a584793895829cb6ccab6c46cc654537df2567
Xavier Rotllan-Puig <xavi.rotllan.puig@gmail.com>
2021-01-07 16:45:54 +0100

    processing ndvi-lts SD

--- download_data.R
+++ download_data.R
@@ -31,14 +31,21 @@ library(ncdf4)
 library(raster)
 nc <- nc_open("ndvi-lts_v2_1km_c_gls_NDVI-LTS_1999-2017-0801_GLOBE_VGT-PROBAV_V2.2.1.nc")
 nc$var$mean
+nc$var$stdev
 
 lon <- ncvar_get(nc, "lon")
 lat <- ncvar_get(nc, "lat")
 # if you have time series
 #time <- ncvar_get(nc, "time") # need to find the right name in 'nc'
-# variable 1
+
+# variable 1: average
 ndvi_lts_1km <- ncvar_get(nc, "mean")
 dim(ndvi_lts_1km)
+
+# variable 2: standard deviation
+ndvi_lts_1km_sd <- ncvar_get(nc, "stdev")
+dim(ndvi_lts_1km_sd)
+
 
 # Copernicus nc files have lat/long belonging to the centre of the pixel, and R uses 
 # upper/left corner. So coordiinates need to be adjusted
@@ -46,6 +53,7 @@ dim(ndvi_lts_1km)
 lon <- lon - (1/112)/2
 lat <- lat + (1/112)/2  
 
+# mean
 #ndvi_lts_1km_rstr <- raster(t(ndvi_lts_1km[, seq(dim(ndvi_lts_1km)[2], 1, -1)]))
 ndvi_lts_1km_rstr <- raster(t(ndvi_lts_1km))
 extent(ndvi_lts_1km_rstr) <- c(range(lon)[1], (range(lon)[2] + (1/112)),
@@ -53,11 +61,53 @@ extent(ndvi_lts_1km_rstr) <- c(range(lon)[1], (range(lon)[2] + (1/112)),
 crs(ndvi_lts_1km_rstr) <- CRS('+init=EPSG:4326')
 ndvi_lts_1km_rstr
 
-writeRaster(ndvi_lts_1km_rstr, filename = "ndvi_lts_1km_rstr.tif", overwrite = TRUE)
-ndvi_lts_1km_rstr <- raster("ndvi_lts_1km_rstr.tif")
 
-plot(ndvi_lts_1km_rstr)
+# SD
+ndvi_lts_1km_rstr_sd <- raster(t(ndvi_lts_1km_sd))
+extent(ndvi_lts_1km_rstr_sd) <- c(range(lon)[1], (range(lon)[2] + (1/112)),
+                                  (range(lat)[1] - (1/112)), range(lat)[2])
+crs(ndvi_lts_1km_rstr_sd) <- CRS('+init=EPSG:4326')
+ndvi_lts_1km_rstr_sd
+
+ndvi_lts_1km_rstr <- brick(ndvi_lts_1km_rstr, ndvi_lts_1km_rstr_sd)
+names(ndvi_lts_1km_rstr) <- c("mean", "sd")
+
+writeRaster(ndvi_lts_1km_rstr, filename = "ndvi_lts_1km_rstr.tif", overwrite = TRUE)
+ndvi_lts_1km_rstr <- brick("ndvi_lts_1km_rstr.tif")
+names(ndvi_lts_1km_rstr) <- c("mean", "sd")
+
+
+plot(ndvi_lts_1km_rstr[[1]])
+plot(ndvi_lts_1km_rstr[[2]])
 nc_close(nc)#
+
+
+ndvi_lts_1km_rstr_vals <- getValues(ndvi_lts_1km_rstr[[1]])
+sum(is.na(ndvi_lts_1km_rstr_vals))
+sum(ndvi_lts_1km_rstr_vals >= 0.936, na.rm = TRUE)
+max(ndvi_lts_1km_rstr_vals, na.rm = TRUE)
+
+ndvi_lts_1km_rstr_vals <- getValues(ndvi_lts_1km_rstr[[2]])
+sum(ndvi_lts_1km_rstr_vals >= 1.016, na.rm = TRUE)
+
+
+ndvi_lts_1km_rstr_clean <- ndvi_lts_1km_rstr$mean
+ndvi_lts_1km_rstr_clean[ndvi_lts_1km_rstr_clean >= 0.936] <- NA
+
+ndvi_lts_1km_rstr_clean_sd <- ndvi_lts_1km_rstr$sd
+ndvi_lts_1km_rstr_clean_sd[ndvi_lts_1km_rstr_clean_sd >= 1.016] <- NA
+plot(ndvi_lts_1km_rstr_clean_sd)
+
+ndvi_lts_1km_rstr_clean <- brick(ndvi_lts_1km_rstr_clean, ndvi_lts_1km_rstr_clean_sd)
+names(ndvi_lts_1km_rstr_clean) <- c("mean", "sd")
+
+
+writeRaster(ndvi_lts_1km_rstr_clean, filename = "ndvi_lts_1km_rstr_clean.tif", overwrite = TRUE)
+ndvi_lts_1km_rstr_clean <- brick("ndvi_lts_1km_rstr_clean.tif")
+names(ndvi_lts_1km_rstr_clean) <- c("mean", "sd")
+
+plot(ndvi_lts_1km_rstr_clean[[1]])
+plot(ndvi_lts_1km_rstr_clean[[2]])
 
 
 
@@ -98,6 +148,17 @@ ndvi_1km_rstr <- raster("ndvi_1km_rstr.tif")
 
 plot(ndvi_1km_rstr)
 nc_close(nc)#
+
+ndvi_1km_rstr_vals <- getValues(ndvi_1km_rstr)
+sum(ndvi_1km_rstr_vals >= 0.936, na.rm = TRUE)
+max(ndvi_1km_rstr_vals, na.rm = TRUE)
+
+ndvi_1km_rstr_clean <- ndvi_1km_rstr
+ndvi_1km_rstr_clean[ndvi_1km_rstr_clean >= 0.936] <- NA
+
+writeRaster(ndvi_1km_rstr_clean, filename = "ndvi_1km_rstr_clean.tif")
+ndvi_1km_rstr_clean <- raster("ndvi_1km_rstr_clean.tif")
+plot(ndvi_1km_rstr_clean)
 
 
 
