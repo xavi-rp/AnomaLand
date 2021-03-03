a536dabf2b4b2364fa99727ae28e636334e64692
ROTLLAN PUIG Xavier (JRC-ISPRA-EXT) <Xavier.ROTLLAN-PUIG@ext.ec.europa.eu>
2021-03-03 11:56:22 +0100

    generalising plot for countries

--- anomalies.R
+++ anomalies.R
@@ -35,21 +35,22 @@ cntry <- "Europe"
 cntry <- "Italy"
 cntry <- "France"
 cntry <- c("Italy", "France")
-cntry <- "Asia"
+cntry <- "Africa"
+avoid_cntry <- "Russia"
 
 
 # World map
 wrld_map <- getMap()
-head(wrld_map)
+#head(wrld_map)
 #unique(wrld_map$continent)
-unique(wrld_map$REGION)
-unique(wrld_map$NAME)
+#unique(wrld_map$REGION)
+#unique(wrld_map$NAME)
 
-selectedMap <- wrld_map[wrld_map$REGION %in% cntry | wrld_map$NAME %in% cntry, ]
-sort(unique(selectedMap$NAME))
-
-extent(selectedMap)
-extent(wrld_map)
+if(!is.null(cntry)) selectedMap <- wrld_map[(wrld_map$REGION %in% cntry | wrld_map$NAME %in% cntry) &
+                                               !wrld_map$NAME %in% avoid_cntry, ]
+#sort(unique(selectedMap$NAME))
+#extent(selectedMap)
+#extent(wrld_map)
 
 
 # Actual NDVI product: dekad 08/01/2019 (month/day/year)
@@ -89,37 +90,48 @@ writeRaster(ndvi_1km_anomalies, "ndvi_1km_anomalies.tif", overwrite = TRUE)
 
 
 # Plotting a map
-jpeg("ndvi_1km_anomalies1.jpg", width = 22, height = 16.5, units = "cm", res = 300)
-par(mar = c(6, 3, 1, 2), bty = "n")
+jpeg("ndvi_1km_anomalies_Fr_It1.jpg", width = 22, height = 16.5, units = "cm", res = 300)
+if(!is.null(cntry)){
+   lgnd_pos <- "right"
+   inst <- -0.35
+}else{ 
+   lgnd_pos <- "bottom"
+   inst <- 0.01
+}
+
+
+par(mar = c(6, 4, 3, 9), bty = "n")
 cuts <- c(minValue(ndvi_1km_anomalies), -anom2, -anom1, anom1, anom2, maxValue(ndvi_1km_anomalies))
 pal <- colorRampPalette(c("springgreen4", "springgreen2", "khaki2", "orange2", "red3"))
 par(xpd = FALSE)
 plot(ndvi_1km_anomalies, breaks = cuts, col = pal(5), legend = FALSE)
 par(xpd = TRUE)
-legend("bottom",
+legend(lgnd_pos,
        legend = c(paste0("High positive anomaly (> ", anom2, ")"),
                   paste0("Low positive anomaly (> ", anom1, ")"),
                   "No anomaly",
                   paste0("Low negative anomaly (< -", anom1, ")"),
                   paste0("High negative anomaly (< -", anom2, ")")),
        fill = pal(5),
-       cex = 1,
-       inset = 0.01)
+       cex = 0.8,
+       #inset = 0.01
+       inset = inst
+       )
 title(main = "NDVI ANOMALIES",
-      outer = TRUE,
-      line = - 3.5,
-      cex.main = 2.5)
+      #outer = TRUE,
+      #line = - 3.5,
+      cex.main = 2)
 
 mtext("Current layer: 'ndvi_v2_1km_c_gls_NDVI_201908010000_GLOBE_PROBAV_V2.2.1.nc'", 
       side = 1, line = 3, 
       #at = 5,
       adj = 0,
-      cex = 1)
+      cex = 0.8)
 mtext("Reference layer: 'ndvi-lts_v2_1km_c_gls_NDVI-LTS_1999-2017-0801_GLOBE_VGT-PROBAV_V2.2.1.nc'", 
       side = 1, line = 4, 
       #at = 5,
       adj = 0,
-      cex = 1)
+      cex = 0.8)
 
 dev.off()
 
